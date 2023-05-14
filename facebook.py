cc516934e4f93554077b6c7d390031f8b8bbfa84
CnRKizo <133593473+CnRKizo@users.noreply.github.com>
2023-05-14 21:26:33 +0300

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -76,10 +76,10 @@ def welcome():
 """
 	total = open(passwordlist,"r")
 	total = total.readlines()
-	print wel 
-	print " [*] Account to crack : {}".format(email)
-	print " [*] Loaded :" , len(total), "passwords"
-	print " [*] Cracking, please wait ...\n\n"
+	print (wel) 
+	print (" [*] Account to crack : {}").format(email)
+	print (" [*] Loaded :" , len(total), "passwords")
+	print (" [*] Cracking, please wait ...\n\n")
 
 	
 if __name__ == '__main__':
